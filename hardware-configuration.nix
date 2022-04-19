836985fe71829851a2a89b78ecf374e482046d37
Snuggle <^-^@snugg.ie>
2022-04-19 12:18:10 +0100

    🎨 Clean up tabs/spaces & reformat

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,48 +4,49 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+	imports =
+	[ 
+		(modulesPath + "/installer/scan/not-detected.nix")
+	];
 
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelParams = [ "nvidia-drm.modeset=1" ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+	boot.kernelPackages = pkgs.linuxPackages_zen;
+	boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
+	boot.initrd.kernelModules = [ ];
+	boot.kernelParams = [ "nvidia-drm.modeset=1" ];
+	boot.kernelModules = [ "kvm-amd" ];
+	boot.extraModulePackages = [ ];
 
-  boot.loader.grub.useOSProber = true;
-  
-  boot.plymouth.enable = false;
+	boot.loader.grub.useOSProber = true;
+	
+	boot.plymouth.enable = false;
 
-  hardware.nvidia.powerManagement.enable = true; # https://nixos.wiki/wiki/Nvidia#Fix_graphical_corruption_on_suspend.2Fresume
+	hardware.nvidia.powerManagement.enable = true; # https://nixos.wiki/wiki/Nvidia#Fix_graphical_corruption_on_suspend.2Fresume
 
-  services.xserver = {
-    videoDrivers = [ "nvidia" ];
-  };
+	services.xserver = {
+		videoDrivers = [ "nvidia" ];
+	};
 
-  hardware.enableAllFirmware = true;
+	hardware.enableAllFirmware = true;
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/5c3bbd25-bf47-4fc2-9059-631bdba52f1d";
-      fsType = "btrfs";
-    };
+	fileSystems."/" = { 
+		device = "/dev/disk/by-uuid/5c3bbd25-bf47-4fc2-9059-631bdba52f1d";
+		fsType = "btrfs";
+	};
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/E266-52DA";
-      fsType = "vfat";
-    };
+	fileSystems."/boot" = { 
+		device = "/dev/disk/by-uuid/E266-52DA";
+		fsType = "vfat";
+	};
 
-  fileSystems."/mnt/Games" =
-    { device = "/dev/disk/by-uuid/917d6f24-1f40-4019-9efa-c9cd6be4c5f6";
-      fsType = "btrfs";
-      options = [ "nofail" "x-systemd.device-timeout=10s" "defaults" ];
-    };
+	fileSystems."/mnt/Games" = { 
+		device = "/dev/disk/by-uuid/917d6f24-1f40-4019-9efa-c9cd6be4c5f6";
+		fsType = "btrfs";
+		options = [ "nofail" "x-systemd.device-timeout=10s" "defaults" ];
+	};
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/488c55c6-d271-4bc4-89b4-63690ee49c24"; }
-    ];
+	swapDevices = [
+		{ device = "/dev/disk/by-uuid/488c55c6-d271-4bc4-89b4-63690ee49c24"; }
+	];
 
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+	hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
