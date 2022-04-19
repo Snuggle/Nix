d7404d9c75512dd7bb4aaf6a523f6f86e5dd050a
Snuggle <^-^@snugg.ie>
2022-04-19 13:02:20 +0100

    💚 Adding Nix Channel to CI

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
 # Originally from https://github.com/gvolpe/nix-config
 
+set +x
+
 build_ci_system() {
+  sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
   cmd="
     nix-build-uncached '<nixpkgs/nixos>' \
       -I nixos-config=system/configuration.nix \
