4f487231c309b9d453f59177c8a862511d3d0eaf
Snuggle <^-^@snugg.ie>
2022-04-19 12:20:28 +0100

    🎨 Remove first indent on Nix files (2)

--- fonts.nix
+++ fonts.nix
@@ -1,37 +1,37 @@
 { pkgs, ... }:
 {
-	enableDefaultFonts = false;
+enableDefaultFonts = false;
 
-	fonts = with pkgs; [
-		# Serif Fonts
-		roboto-slab
+fonts = with pkgs; [
+	# Serif Fonts
+	roboto-slab
 
-		# Sans-serif Fonts
-		fontforge
-		source-sans # Previously 'source-sans-pro'
-		source-serif # Previously 'source-serif-pro'
-		noto-fonts
-		ubuntu_font_family
+	# Sans-serif Fonts
+	fontforge
+	source-sans # Previously 'source-sans-pro'
+	source-serif # Previously 'source-serif-pro'
+	noto-fonts
+	ubuntu_font_family
 
-		# Mono Fonts
-		source-code-pro
-		fantasque-sans-mono
+	# Mono Fonts
+	source-code-pro
+	fantasque-sans-mono
 
-		# Emoji Fonts
-		noto-fonts-emoji-blob-bin
+	# Emoji Fonts
+	noto-fonts-emoji-blob-bin
 
-		# Non-English Fonts
-		noto-fonts-cjk
+	# Non-English Fonts
+	noto-fonts-cjk
 
-		(nerdfonts.override { fonts = [ "FantasqueSansMono" "SourceCodePro" ]; })
-	];
+	(nerdfonts.override { fonts = [ "FantasqueSansMono" "SourceCodePro" ]; })
+];
 
-	fontconfig = {
-		defaultFonts = {
-			serif = [ "Source Serif 4" "Roboto Slab" "Ubuntu" ];
-			sansSerif = [ "Source Sans 3" "Ubuntu" ];
-			monospace = [ "Fantasque Sans Mono" "Source Code Pro" "Ubuntu Mono" ];
-			emoji = [ "Blobmoji" ];
-			};
+fontconfig = {
+	defaultFonts = {
+		serif = [ "Source Serif 4" "Roboto Slab" "Ubuntu" ];
+		sansSerif = [ "Source Sans 3" "Ubuntu" ];
+		monospace = [ "Fantasque Sans Mono" "Source Code Pro" "Ubuntu Mono" ];
+		emoji = [ "Blobmoji" ];
 		};
+	};
 }
