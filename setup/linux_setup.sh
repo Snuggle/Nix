8019962d0fb14dfc19354ed3e88148a42f1c05fd
Snuggle <^-^@snugg.ie>
2022-07-15 20:31:59 +0100

    ✅ Everywhere installs from the same place

--- setup/linux_setup.sh
+++ setup/linux_setup.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 echo "❄️ Installing Nix…"
+source /etc/lsb-release
+if [ "$DISTRIBID" == "nixos" ]; then
+	rm -rfv /etc/nixos/
+	git clone https://github.com/Snuggle/NixOS /etc/nixos
+	cd /etc/nixos
+	git remote remove origin
+	git remote add origin git@github.com:Snuggle/nixos
+else
+	rm -rfv ~/.nixpkgs
+	git clone https://github.com/Snuggle/NixOS ~/.nixpkgs
+	cd ~/.nixpkgs
+	git remote remove origin
+	git remote add origin git@github.com:Snuggle/nixos
+	sh <(curl -L https://nixos.org/nix/install) --daemon
+fi
 sh <(curl -L https://nixos.org/nix/install) --daemon
 echo "❄️ Nix is installed!"
