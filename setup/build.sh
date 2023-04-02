b837b9a4cbd5c3e97696700815220bc2c02abc48
Snuggle <^-^@snugg.ie>
2023-04-02 02:09:05 +0100

    Revert "⚗️ Experiment with build order"
    This reverts commit 2c6fcfa4d0d382dfffb2241a0181e4e1e9136cb1.

--- setup/build.sh
+++ setup/build.sh
@@ -21,10 +21,10 @@ build_ci_system() {
 
 build_nixos_unstable_system() {
   echo "🔨 Building nixos_unstable"
-  ./switch
   sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
   sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
   sudo nix-channel --update
+  ./switch
   sed -i 's/"nvidia"//g' hardware-configuration.nix
   sed -i 's/boot.kernelPackages = pkgs.linuxPackages_zen;//g' hardware-configuration.nix
   echo  "🧪 Testing system configuration…"
@@ -35,10 +35,10 @@ build_nixos_unstable_system() {
 
 build_nixos_stable_system() {
   echo "🔨 Building nixos_stable"
-  ./switch
   sudo nix-channel --add https://nixos.org/channels/nixos-$NIX_BUILD_VERSION nixos
   sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-$NIX_BUILD_VERSION.tar.gz home-manager
   sudo nix-channel --update
+  ./switch
   sed -i 's/"nvidia"//g' hardware-configuration.nix
   sed -i 's/boot.kernelPackages = pkgs.linuxPackages_zen;//g' hardware-configuration.nix
   echo "🧪 Testing system configuration…"
@@ -49,10 +49,10 @@ build_nixos_stable_system() {
 
 build_darwin_unstable_system() {
   echo "🔨 Building darwin_unstable"
-  ./switch
   sudo nix-channel --add http://nixos.org/channels/nixpkgs-unstable nixpkgs
   sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
   sudo nix-channel --update
+  ./switch
   echo "🧪 Testing system configuration…"
   source /etc/static/bashrc
   darwin-rebuild build --dry-run
@@ -61,7 +61,6 @@ build_darwin_unstable_system() {
 
 build_darwin_stable_system() {
   echo "🔨 Building darwin_stable"
-  ./switch
   sudo nix-channel --add http://nixos.org/channels/nixpkgs-$NIX_BUILD_VERSION-darwin nixpkgs
   sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-$NIX_BUILD_VERSION.tar.gz home-manager
   sudo nix-channel --update
