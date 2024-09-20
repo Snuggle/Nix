9e62163c041dc03af27fa6a525661fa05f0d5f2a
Snuggle <^-^@snugg.ie>
2024-09-20 12:11:45 +0100

    :truck: Rename common directory

--- hosts/cherry/configuration.nix
+++ hosts/cherry/configuration.nix
@@ -8,10 +8,10 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../common/configuration.nix
+      ../!common/configuration.nix
     ];
 
-  hostName = "cherry"; # Define your hostname.
+  networking.hostName = "cherry"; # Define your hostname.
     
   system.stateVersion = "24.05"; # Did you read the comment?
 
